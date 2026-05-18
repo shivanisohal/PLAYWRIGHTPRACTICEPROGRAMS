42029395d95ef926c856681ff1c73037f3acd6cb
Shivanisohal <sohalshivani2@gmail.com>
2026-05-18 08:07:58 +0530

    Add ternary operator practice files

---
+++ javascriptBasic/ternaryOp.js
@@ -0,0 +1,3 @@
+let user=18;
+let user_will_go_goa=user>18?"will go":"will not go";
+console.log(user_will_go_goa);
---
+++ javascriptBasic/ternaryope2between2numbers.js
@@ -0,0 +1,10 @@
+let a= 30;
+let b=60;
+
+let max= a>b?"A is greater":"B is greater";
+console.log(max);
+
+
+let c=90;
+ let maxValue=a>b?(a>c?"A is greater":"C is greater"):(b>c?"b is greater":"c is greater");
+ console.log(maxValue);
