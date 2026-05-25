6218a33720905bd02836762caca11f883238a994
Shivanisohal <sohalshivani2@gmail.com>
2026-05-25 06:47:24 +0530

    Add while and do-while loop practice file
    - Add whileloop.js with while and do-while loop examples

---
+++ javascriptBasic/whileloop.js
@@ -0,0 +1,22 @@
+let i=1;
+while(i<=5)
+{
+console.log(i);
+i++;
+}
+
+console.log("---------------------")
+
+let j=1
+do{
+    console.log(j);
+    j++;
+}while(j<=6);
+
+console.log("---------------------");
+
+let k=1
+do{
+    console.log(k);
+    k++;
+}while(k<1);
